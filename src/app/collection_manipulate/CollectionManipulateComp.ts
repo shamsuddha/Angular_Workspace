356bce30e2f796679232fa329af6d26a7b499b42
shaiful-egen <115973748+shaiful-egen@users.noreply.github.com>
2023-04-09 01:05:29 +0600

    data stucture

--- src/app/collection_manipulate/CollectionManipulateComp.ts
+++ src/app/collection_manipulate/CollectionManipulateComp.ts
@@ -1,10 +1,33 @@
 import {Component, OnInit} from "@angular/core";
 
+
+class NestDto {
+  id: number | null = null;
+  location: string | null = null;
+
+  constructor(o?: Partial<NestDto>) {
+    Object.assign(this, o);
+  }
+}
+
+class EggDto {
+  id: number | null = null;
+  size: string | null = null;
+
+  constructor(o?: Partial<EggDto>) {
+    Object.assign(this, o);
+  }
+}
+
 class BirdDto {
 
   id: number | null = null;
   name: string | null = null;
   age: number | null = null;
+  aliveFlag: boolean | null = null;
+
+  nestDto: NestDto | null = null;
+  eggDtoList: Array<EggDto> = []
 
   constructor(o?: Partial<BirdDto>) {
     Object.assign(this, o);
@@ -24,15 +47,23 @@ export class CollectionManipulateComp implements OnInit {
   birdDtoList: Array<BirdDto> = [];
 
   ngOnInit(): void {
-    for (let i = 0; i < 10; i++) {
+
+    for (let i = 1; i <= 10; i++) {
       const birdDto: BirdDto = new BirdDto({id: i, name: "name " + i, age: 10 + i});
+      const eggDtoList: Array<EggDto> = this.getEggDto(birdDto)
+      const nestDto: NestDto = new NestDto({id: 0, location: ""});
+      birdDto.eggDtoList = eggDtoList;
+      birdDto.nestDto = nestDto;
       this.birdDtoList = [...this.birdDtoList, JSON.parse(JSON.stringify(birdDto))];
     }
     console.log(this.birdDtoList)
   }
 
+  private getEggDto(birdDto: BirdDto): Array<EggDto> {
+    return [];
+  }
+
   showSeniorBird() {
-    const t: Array<BirdDto> = this.birdDtoList
     for (let i = 0; i < this.birdDtoList.length; i++) {
       const birdDtoT: BirdDto = this.birdDtoList[i];
       if (birdDtoT.age && birdDtoT.age > 15) {
@@ -40,4 +71,6 @@ export class CollectionManipulateComp implements OnInit {
       }
     }
   }
+
+
 }
