f9ca342ad8b528f5be0d684a2f404f4dedc00c08
shaiful-egen <115973748+shaiful-egen@users.noreply.github.com>
2023-04-22 23:35:23 +0600

    8

--- src/app/simple_social_app/comp/SocialAppComp.ts
+++ src/app/simple_social_app/comp/SocialAppComp.ts
@@ -1,15 +1,16 @@
-import { PostLikeDtoApiService } from './../dto_api_service/PostLikeDtoApiService';
-import { tap } from 'rxjs';
-import { filter } from 'rxjs';
-import { PostDto } from './../dto/PostDto';
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { PostDtoApiService } from "../dto_api_service/PostDtoApiService";
-import { HttpClient } from "@angular/common/http";
-import { UserDto } from '../dto/UserDto';
-import { CommentDto } from '../dto/CommentDto';
-import { PostLikeDto } from '../dto/PostLikeDto';
-import { PostSearchDto } from "../dto/request/PostSearchDto";
+import {PostLikeDtoApiService} from './../dto_api_service/PostLikeDtoApiService';
+import {tap} from 'rxjs';
+import {filter} from 'rxjs';
+import {PostDto} from './../dto/PostDto';
+import {Component, OnInit} from "@angular/core";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {PostDtoApiService} from "../dto_api_service/PostDtoApiService";
+import {HttpClient} from "@angular/common/http";
+import {UserDto} from '../dto/UserDto';
+import {CommentDto} from '../dto/CommentDto';
+import {PostLikeDto} from '../dto/PostLikeDto';
+import {PostSearchDto} from "../dto/request/PostSearchDto";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'SocialAppComp',
@@ -22,26 +23,31 @@ export class SocialAppComp implements OnInit {
 
   postDtoFg = new FormGroup({
     body: new FormControl<string | null>(null, [Validators.required,
-    Validators.maxLength(300), Validators.minLength(3)]),
+      Validators.maxLength(300), Validators.minLength(3)]),
   });
 
   postDtoList: Array<PostDto> = [];
   commentDtoList: Array<CommentDto> = [];
+  currentUserId: number | null = null;
 
   constructor(
     private postDtoApiService: PostDtoApiService,
     private postLikeDtoApiService: PostLikeDtoApiService,
+    private activatedRoute: ActivatedRoute,
     private http: HttpClient) {
   }
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((e) => {
+      this.currentUserId = +e['userId'];
+    })
     this.searchPost();
   }
 
   savePost() {
     console.log(this.postDtoFg.value);
     const postDto = new PostDto(this.postDtoFg.value)
-    postDto.userDto = new UserDto({ id: 2, name: "name 2" });
+    postDto.userDto = new UserDto({id: this.currentUserId, name: "name " + this.currentUserId});
     this.postDtoApiService.savePostDto(postDto)
       .subscribe((e) => {
         this.searchPost();
@@ -50,7 +56,7 @@ export class SocialAppComp implements OnInit {
 
   postLikeClicked(postDto: PostDto) {
     const postLikeDto = new PostLikeDto()
-    postLikeDto.userDto = new UserDto({ id: 2, name: "name 2" });
+    postLikeDto.userDto = new UserDto({id: this.currentUserId, name: "name " + this.currentUserId});
     postLikeDto.postDto = new PostDto(postDto);
     this.postLikeDtoApiService.savePostLikeDto(postLikeDto)
       .subscribe((e) => {
@@ -63,8 +69,7 @@ export class SocialAppComp implements OnInit {
   }
 
   searchPost() {
-    this.postDtoApiService.search(new PostSearchDto({ userId: 2 })).
-    subscribe((e: Array<PostDto>) => {
+    this.postDtoApiService.search(new PostSearchDto()).subscribe((e: Array<PostDto>) => {
       this.postDtoList = e;
     })
   }
@@ -76,7 +81,7 @@ export class SocialAppComp implements OnInit {
   onShowClick(postDto: PostDto) {
     // console.log(postDto)
     // let singlePost : PostDto = ;
-    this.postDtoApiService.search(new PostSearchDto({ userId: 2 }))
+    this.postDtoApiService.search(new PostSearchDto({userId: 2}))
       .pipe(
         filter((e: Array<PostDto>) => {
           // console.log(postDto.id);
@@ -90,8 +95,8 @@ export class SocialAppComp implements OnInit {
         tap((e) => console.log(e))
       )
       .subscribe((e: Array<PostDto>) => {
-        this.postDtoList = e;
-      }
+          this.postDtoList = e;
+        }
       )
   }
 }
