0aee8e1e8b82848838609be31ff974bfd6fe2909
shamsuddha <shamsuddha@gmail.com>
2023-08-27 11:55:44 +0600

    ss

---
+++ src/app/lion/LionComp.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+
+@Component({
+  selector: 'LionComp',
+  templateUrl: './LionComp.html',
+  styleUrls: ['./LionComp.scss'],
+})
+export class LionComp {
+
+ lionDtoFg = new FormGroup({
+
+  });
+}
