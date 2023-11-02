65ce58cf0dc5cda42a9c71c139130d27da43df83
shamsuddha <shamsuddha@gmail.com>
2023-11-02 15:21:28 +0600

    dd

--- src/app/rxjs_example/RxjsExample2Comp.ts
+++ src/app/rxjs_example/RxjsExample2Comp.ts
@@ -1,14 +1,14 @@
 import { CrudPracticeOneComp } from './../crud_practice_one/CrudPracticeOneComp';
 import { Component } from '@angular/core';
-import { filter, map, Observable, subscribeOn, Subscriber, tap } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { filter, map, Observable, subscribeOn, Subscriber, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 class StudentDto {
   id: number = 0;
-  name: string = "";
+  name: string = '';
 
   constructor(o?: Partial<StudentDto>) {
-    Object.assign(this, o)
+    Object.assign(this, o);
   }
 }
 
@@ -16,23 +16,18 @@ class StudentDto {
   selector: 'RxjsExample2Comp',
   standalone: true,
   template: `
-    <button (click)="click2()">click2</button>
+    <button (click)="click1()">click2</button>
+    <button (click)="click2()">click3</button>
     <button (click)="click3()">click3</button>
-    <button (click)="click3()">click3</button>
-    <button (click)="click3()">click3</button>
-    <button (click)="click3()">click3</button>
-    <button (click)="click3()">click3</button>
-    <button (click)="click3()">click3</button>
-    <button (click)="click3()">click3</button>
-   
+    <button (click)="click4()">click3</button>
+    <button (click)="click5()">click3</button>
+    <button (click)="click6()">click3</button>
+    <button (click)="click7()">click3</button>
+    <button (click)="click8()">click3</button>
   `,
 })
-
 export class RxjsExample2Comp {
-
-
-  constructor() {
-  }
+  constructor() {}
 
   click2() {
     this.rajitApi()
@@ -40,18 +35,17 @@ export class RxjsExample2Comp {
         filter((e: Array<StudentDto>) => {
           if (e.find((t: StudentDto) => t.id == 4)) {
             return false;
-          }
-          else {
+          } else {
             return true;
           }
         }),
         map((e: Array<StudentDto>) => {
           e.map((t: StudentDto) => {
-            t.name = t.name.toUpperCase()
-            return t
-          })
+            t.name = t.name.toUpperCase();
+            return t;
+          });
           return e;
-        }),
+        })
         //tap((e:Array<StudentDto>)=> console.log(e))
       )
       .subscribe((e: Array<StudentDto>) => console.log(e));
@@ -60,66 +54,67 @@ export class RxjsExample2Comp {
   fakeApi(): Observable<Array<StudentDto>> {
     const t: Observable<Array<StudentDto>> = new Observable((subscriber) => {
       setTimeout(() => {
-        subscriber.next([new StudentDto({ id: 1, name: "name 1" }), new StudentDto({ id: 2, name: "name 2" })]);
+        subscriber.next([
+          new StudentDto({ id: 1, name: 'name 1' }),
+          new StudentDto({ id: 2, name: 'name 2' }),
+        ]);
       }, 2000);
       setTimeout(() => {
-        subscriber.next([new StudentDto({ id: 3, name: "name 3" }), new StudentDto({ id: 4, name: "name 4" })]);
+        subscriber.next([
+          new StudentDto({ id: 3, name: 'name 3' }),
+          new StudentDto({ id: 4, name: 'name 4' }),
+        ]);
       }, 3000);
-
-    })
+    });
     return t;
   }
 
   rajitApi(): Observable<Array<StudentDto>> {
     const t: Observable<Array<StudentDto>> = new Observable((subscriber) => {
-      subscriber.next([new StudentDto({ id: 4, name: "name 1" }), new StudentDto({ id: 2, name: "name 2" })]);
+      subscriber.next([
+        new StudentDto({ id: 4, name: 'name 1' }),
+        new StudentDto({ id: 2, name: 'name 2' }),
+      ]);
     });
     return t;
   }
 
-
   newApi(): Observable<Array<any>> {
     const t: Observable<Array<any>> = new Observable((subscriber) => {
-      subscriber.next(["kkjjTYYTF", "TTTThjgjgj"])
-      subscriber.next(["kkjjTYYTF", 1,23,2323,"fff"])
-    })
+      subscriber.next(['kkjjTYYTF', 'TTTThjgjgj']);
+      subscriber.next(['kkjjTYYTF', 1, 23, 2323, 'fff']);
+    });
     return t;
-
   }
 
   newApi2(): Observable<Array<StudentDto>> {
-
     const t: Observable<Array<StudentDto>> = new Observable((Subscriber) => {
-      Subscriber.next([{ id: 1, name: "name 1" }, { id: 2, name: "name 2" }]);
-    })
+      Subscriber.next([
+        { id: 1, name: 'name 1' },
+        { id: 2, name: 'name 2' },
+      ]);
+    });
 
     return t;
 
     // t.pipe(
     //   tap((e: Array<StudentDto>)=> console.log(e))
     // ).subscribe((e:Array<StudentDto>)=> console.log(e))
-
   }
 
-
   click3() {
-
     this.newApi()
       .pipe(
         filter((e: Array<any>) => e.length == 2),
         map((e: Array<any>) => {
-
-          const j:any = e.concat(10)
-         // console.log(j);
-         return j;
-
+          const j: any = e.concat(10);
+          // console.log(j);
+          return j;
         }),
-        tap((e)=> console.log(e))
+        tap((e) => console.log(e))
       )
       .subscribe((e: Array<any>) => console.log(e));
-
   }
-
 
   // clicke(){
   //   this.newApi()
@@ -129,6 +124,4 @@ export class RxjsExample2Comp {
   //   )
   //   .subscribe((e:Array<any>)=> console.log(e))
   // }
-
-
 }
